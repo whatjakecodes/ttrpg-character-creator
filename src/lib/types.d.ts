694e04b0249e7876a26d0c6ddec734578b20da80
Jake Stout <kyjstout@gmail.com>
2024-12-18 19:06:39 -0500

    Initial commit

---
+++ src/lib/types.d.ts
@@ -0,0 +1,6 @@
+﻿export interface CharacterClass {
+  name: 'cleric'|'druid'|'barbarian'|'paladin'|'warlock'
+}
+export interface CharacterCreatorSettings {
+  characterClasses: Array<CharacterClass>;
+}
